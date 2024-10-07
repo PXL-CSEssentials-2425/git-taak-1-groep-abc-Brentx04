55d1e60cdba3553cfedf7b6448c46a0c0dab6f40
brentdaenen <Brent.Daenen@student.pxl.be>
2024-10-07 21:32:32 +0200

    commit: KLEUR-03

--- GitTaak1/GitTaak1/MainWindow.xaml.cs
+++ GitTaak1/GitTaak1/MainWindow.xaml.cs
@@ -20,5 +20,55 @@ namespace GitTaak1
         {
             InitializeComponent();
         }
+
+        private void pxlPNG_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Black);
+        }
+
+        private void pxlPNG_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void Image_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void Image_MouseEnter_1(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Yellow);
+        }
+
+        private void Image_MouseLeave_1(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void Image_MouseEnter_2(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Green);
+        }
+
+        private void Image_MouseLeave_2(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
+
+        private void Image_MouseEnter_3(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void Image_MouseLeave_3(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = new SolidColorBrush(Colors.LightGray);
+        }
     }
 }
